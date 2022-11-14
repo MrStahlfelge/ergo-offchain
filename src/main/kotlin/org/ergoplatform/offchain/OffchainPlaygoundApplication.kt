466fd4682961e2749895bfc2e87fd7247562302d
Benjamin Schulte <bs@golfgl.de>
2022-11-14 15:02:54 +0100

    Init

---
+++ src/main/kotlin/org/ergoplatform/offchain/OffchainPlaygoundApplication.kt
@@ -0,0 +1,11 @@
+package org.ergoplatform.offchain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OffchainPlaygoundApplication
+
+fun main(args: Array<String>) {
+	runApplication<OffchainPlaygoundApplication>(*args)
+}
---
+++ src/test/kotlin/org/ergoplatform/offchain/OffchainPlaygoundApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ergoplatform.offchain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OffchainPlaygoundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
